72c8c41280001986f9bf20a61784ae611109839f
yaolidong <yao401405972@gmail.com>
2021-06-30 12:01:02 +0800

    Reset

---
+++ cmake-build-debug/CMakeFiles/K--CA.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/Message.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/Message.cpp.o"
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/Network.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/Network.cpp.o"
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/Node.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/Node.cpp.o"
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/Pbft.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/Pbft.cpp.o"
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/ViewState.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/ViewState.cpp.o"
+  "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/sha256.cpp" "/Users/yao/Documents/GitHub/desktop-tutorial/K--CA/cmake-build-debug/CMakeFiles/K--CA.dir/sha256.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/K--CA.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/K--CA.dir/Message.cpp.o"
+  "CMakeFiles/K--CA.dir/Network.cpp.o"
+  "CMakeFiles/K--CA.dir/Node.cpp.o"
+  "CMakeFiles/K--CA.dir/Pbft.cpp.o"
+  "CMakeFiles/K--CA.dir/ViewState.cpp.o"
+  "CMakeFiles/K--CA.dir/sha256.cpp.o"
+  "K--CA"
+  "K--CA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/K--CA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
